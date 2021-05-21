0ee0f00a481fe5a9a9dbe842dd22a4f9448e1678
Giovanni Pires <gpires@ebay.com>
2021-05-21 16:15:07 -0300

    Testing eBay with slowMo 50 milliseconds

--- slow-mo.js
+++ slow-mo.js
@@ -2,5 +2,7 @@ const { firefox } = require('playwright');
 
 (async () => {
   const browser = await firefox.launch({ headless: false, slowMo: 50 });
+  const page = await browser.newPage();
+  await page.goto('https://www.ebay.com/');
   await browser.close();
 })();
