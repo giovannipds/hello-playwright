7d73be43aa544293a56fbd1ca53e45ba030d4f1b
Giovanni Pires <gpires@ebay.com>
2021-05-21 16:54:38 -0300

    Context sample

---
+++ contexts.js
@@ -0,0 +1,16 @@
+const { chromium, devices } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext({
+    ...devices['Pixel 2'],
+    permissions: ['geolocation'],
+    geolocation: { longitude: -122.4786, latitude: 37.8202 },
+  });
+  const page = await context.newPage();
+  await page.goto('https://openstreetmap.org');
+  await page.click('.control-locate');
+  await page.waitForLoadState('networkidle');
+  await page.screenshot({ path: 'osm.png' });
+  await browser.close();
+})();
