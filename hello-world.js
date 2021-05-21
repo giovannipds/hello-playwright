90cc7ee382c6921fe0e34af37fe26b6f793ba543
Giovanni Pires <gpires@ebay.com>
2021-05-21 16:04:23 -0300

    Force it to open the browser

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,6 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false });
   await browser.close();
 })();
