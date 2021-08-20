cbd452ae720283c42cb45243582f9491a3c4fae7
Giovanni Pires <gpires@ebay.com>
2021-08-20 19:21:50 -0300

    Fix test to see the error

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,17 +5,17 @@ test('my test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Playwright');
+  await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(page.locator('text=Get Started')).toHaveAttribute('href', '/docs/intro');
+  await expect(page.locator('text="Get started"')).toHaveAttribute('href', '/docs/intro');
 
   // Expect an element "to be visible".
-  await expect(page.locator('text=Learn more')).toBeVisible();
+  await expect(page.locator('text=Learn more >> nth=0')).toBeVisible();
 
-  await page.click('text=Get Started');
+  await page.click('text="Get started"');
   // Expect some text to be visible on the page.
-  await expect(page.locator('text=System requirements')).toBeVisible();
+  await expect(page.locator('text=Installation >> nth=0')).toBeVisible();
 
   // Compare screenshot with a stored reference.
   expect(await page.screenshot()).toMatchSnapshot('get-started.png');
